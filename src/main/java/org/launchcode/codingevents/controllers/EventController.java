7c40c90769d34245f3570b27107647a71871611e
Meg <40369841+mcullins@users.noreply.github.com>
2020-02-15 08:40:24 -0600

    preparing for merge

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Chris Bay
@@ -19,23 +21,62 @@ public class EventController {
 
     private static List<String> events = new ArrayList<>();
 
+//    @GetMapping
+//    public String displayAllEvents(Model model) {
+//        model.addAttribute("title", "All Events");
+//        model.addAttribute("events", events);
+//        return "events/index";
+//    }
+
     @GetMapping
-    public String displayAllEvents(Model model) {
-        model.addAttribute("title", "All Events");
+    public String displayEventsList(Model model){
+        Map<String, String> events = new HashMap<>();
+        events.put("LC101", "LaunchCode entry class");
+        events.put("Planet Comicon KC", "Kansas City comic convention");
+        events.put("LC Meetup", "Meet other students to work on projects together");
         model.addAttribute("events", events);
+
+        List<String> images = new ArrayList<>();
+        images.add("/comicon.jpg");
+        images.add("/lc.jpg");
+        images.add("/plaza.jpg");
+        model.addAttribute("images", images);
+
         return "events/index";
     }
+//
+//    @GetMapping
+//    public String getImages(Model model){
+//        List<String> images = new ArrayList<>();
+//        images.add("comicon.jpg");
+//        images.add("lc.jpg");
+//        images.add("plaza.jpg");
+//        model.addAttribute("images", images);
+//        return "events/index";
+//    }
 
     @GetMapping("create")
-    public String displayCreateEventForm(Model model) {
-        model.addAttribute("title", "Create Event");
+    public String renderCreateEventForm(){
         return "events/create";
     }
 
+
+//    @GetMapping("create")
+//    public String displayCreateEventForm(Model model) {
+//        model.addAttribute("title", "Create Event");
+//        return "events/create";
+//    }
+
     @PostMapping("create")
-    public String processCreateEventForm(@RequestParam String eventName) {
+    public String createEvent(@RequestParam String eventName){
         events.add(eventName);
         return "redirect:";
     }
+
+//    @PostMapping("create")
+//    public String processCreateEventForm(@RequestParam String eventName) {
+//        events.add(eventName);
+//        return "redirect:";
+//    }
 
 }
